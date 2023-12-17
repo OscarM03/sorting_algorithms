5ff0083a2504235eb708f376b78d87a51b3e9823
OscarM03 <oscarmutwiri03@gmail.com>
2023-12-17 19:52:53 +0300

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -43,9 +43,9 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
 				flag = 1;
 			}
-			print_array(array, size);
 		}
 		if (flag == 0)
 		{
