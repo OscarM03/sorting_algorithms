90994da51c280352f9b869343fbc6648967102df
OscarM03 <oscarmutwiri03@gmail.com>
2023-12-17 19:37:15 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -32,5 +32,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int lb, int ub, size_t size);
 
 #endif
